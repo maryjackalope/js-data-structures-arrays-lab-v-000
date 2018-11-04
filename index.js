2bed6835073e815487f7c4cd375bb47ee39cd453
Mary Jack <maryjackinthebox@gmail.com>
2018-11-04 23:15:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,12 +18,15 @@ function destructivelyRemoveFirstDriver(){
   drivers.shift()
 }
 
-function 
-/*function removeLastDriver(){
-  drivers.slice(-1)
+function appendDriver(name) {
+  return [...drivers, name];
 }
-
-function removeFirstDriver(){
-  drivers.slice(1)
+ function prependDriver(name) {
+  return [name, ...drivers];
 }
-*/
+ function removeLastDriver() {
+  return drivers.slice(0, drivers.length - 1);
+}
+ function removeFirstDriver() {
+return drivers.slice(1);
+}
